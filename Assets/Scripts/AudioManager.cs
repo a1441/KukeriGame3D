819c24ea962925efe616147d825608389a0acdc4
FruitBasket <b.r.markov@gmail.com>
2026-02-01 13:03:44 +0200

    final

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,36 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Audio;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.Audio;
 
-public enum SoundType
+// public enum SoundType
 
-{
-    Attack,
-    Hit,
-    Death,
-    Walk
-}
-[RequireComponent(typeof(AudioSource))]
-public class AudioManager : MonoBehaviour
-{
-    [SerializeField] private AudioClip[] _soundList;
-    private static AudioManager _instance;
-    private AudioSource _audioSource;
+// {
+//     Attack,
+//     Hit,
+//     Death,
+//     Walk
+// }
+// [RequireComponent(typeof(AudioSource))]
+// public class AudioManager : MonoBehaviour
+// {
+//     [SerializeField] private AudioClip[] _soundList;
+//     private static AudioManager _instance;
+//     private AudioSource _audioSource;
 
-    private void Awake()
-    {
-        _instance = this;
-    }
+//     private void Awake()
+//     {
+//         _instance = this;
+//     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        _audioSource = GetComponent<AudioSource>();
-    }
+//     // Start is called once before the first execution of Update after the MonoBehaviour is created
+//     void Start()
+//     {
+//         _audioSource = GetComponent<AudioSource>();
+//     }
 
-    public static void PlaySound(SoundType sound, float volume = 1.0f)
-    {
-        _instance._audioSource.PlayOneShot(_instance._soundList[(int)sound], volume);
-    }
-}
+//     public static void PlaySound()
+//     {
+//         // _instance._audioSource.PlayOneShot(_instance._soundList[(int)sound], volume);
+//     }
+// }
--- Assets/Scripts/CharacterStats.cs
+++ Assets/Scripts/CharacterStats.cs
@@ -35,7 +35,7 @@ public class CharacterStats : MonoBehaviour
 
         currentHealth -= amount;
 
-        AudioManager.PlaySound(SoundType.Hit, 0.7f);
+        // AudioManager.PlaySound(SoundType.Hit, 0.7f);
 
         if (_hitFlash != null)
             _hitFlash.Flash();
